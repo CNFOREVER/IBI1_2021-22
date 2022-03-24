86d9d36450d3d0e59446721c6ca69c7db12a1189
CNFOREVER <101691582+CNFOREVER@users.noreply.github.com>
2022-03-24 12:09:47 -0500

    Create variables.py

---
+++ Practical 5/variables.py
@@ -0,0 +1,20 @@
+a = 19245301  # 2022
+b = 4218520  # 2021
+c = 271  # 2020
+d = b-c
+e = a-b
+if d > e:
+    print("d is greater")
+if d < e:
+    print("e is greater")
+f = e/b  # rate  of new cases in 2021
+g = d/c  # rate  of new cases in 2020
+if f > g:
+    print("rate of new cases in 2021 is greater")
+if f < g:
+    print("rate of new cases in 2020 is greater")
+
+
+X = input() == "True"
+Y = input() == "False"
+W = X and Y #The value will be false
