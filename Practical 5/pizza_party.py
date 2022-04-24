f8d3f2857489cb9992035ded45ca87d0df92eb13
CNFOREVER <101691582+CNFOREVER@users.noreply.github.com>
2022-04-24 22:08:13 +0800

    Update pizza_party.py

--- Practical 5/pizza_party.py
+++ Practical 5/pizza_party.py
@@ -1,3 +1,16 @@
+#First, initialize the slices'number as p and cut times as n
+#Then, use the while loop to find the 64 slices needs how many cut times are
+
+#initialize the p and n from 0
+#use the while loop with the functional relationship between p and n to find the answer
+#print the answer
+
+
+
+
+
+
+
 p = 0
 n = 0
 while p <= 64:
