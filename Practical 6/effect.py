a46839d6e1f56d7f0e252307244978eb0cf98db4
CNFOREVER <101691582+CNFOREVER@users.noreply.github.com>
2022-04-24 22:24:08 +0800

    Update effect.py

--- Practical 6/effect.py
+++ Practical 6/effect.py
@@ -1,3 +1,10 @@
+#import matplotlib
+#creat a new dictionary to contain the information in two lists
+# print the dictionary
+# draw the plot 
+
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -5,7 +12,9 @@ parental_age = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75]
 chd = [1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94]
 print(parental_age)  # question 1
 print(chd)  # question 1
-plt.scatter(parental_age, chd, marker='o')  # question 2
+plt.scatter(parental_age, chd, marker='o') # question 2
+plt.xlabel("Paternal age")
+plt.ylabel("Risk for CHD")
 plt.show()  # question 2
 n = int(input())
 i = 0
